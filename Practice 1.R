fe4aaf79f6c4f87557658c8694776c49aea8ec1c
Roum Lee <81roumiiii@gmail.com>
2026-07-25 09:30:15 -0700

    Changed direction to applying K-classification analysis on Breast cancer data

--- Practice 1.R
+++ Practice 1.R
@@ -1,13 +1,42 @@
-install.packages("tidyverse")
-install.packages("janitor")
+install.packages("tidyverse") 
 library(tidyverse)
-library(janitor)
 
-raw_ihc_data <- read_tsv("practice_data/normal_ihc_data.tsv")
-raw_ihc_data <- clean_names(raw_ihc_data)
+#loading dataset and changing column names
+protein_data <- read_csv("practice_data/protein_dataset/proteinas.csv", col_names = c("protein_id", "sequence",
+                                                                                      "molecular_weight", "isoelectric_point",
+                                                                                      "hydrophobicity", "total_charge",
+                                                                                      "polar_proportion", "nonpolar_proportion",
+                                                                                      "sequence_length", "class"), skip = 1)
+#Changing the spanish class names into english
+protein_data <- protein_data |>
+  mutate(class = recode(class,
+                        "Receptora" = "Receptor",
+                        "Estrutural" = "Structural", 
+                        "Enzima" = "Enzyme", 
+                        "Transporte" = "Transport",
+                        "Outras" = "Other"))
 
-ggplot(data = toothgrowth_data, aes(x = vitamin_dose, y = tooth_length)) + 
-  geom_point() + 
-  xlab("Vitamin Dose") + 
-  ylab("Tooth Length") + 
-  theme(text = element_text(size = 10))
+#plotting total charge to nonpolar proportion depending on class of protein
+plot_prac <- ggplot(protein_data, aes(x = total_charge, y = nonpolar_proportion)) + 
+  geom_point(aes(colour = class, shape = class)) + 
+  labs(x = "Total Charge", y = "Nonpolar Proportion", shape = "Class of Protein", colour = "Class of Protein") +
+  facet_grid(rows = vars(class)) +
+  theme(text = element_text(size = 8))
+plot_prac
+
+
+#creating low/medium/high category column for MW
+protein_data <- protein_data |>
+  mutate(mw_category = cut(molecular_weight, 
+                           breaks = 3,
+                           labels = c("Low", "Medium", "High")))
+
+#selecting for MW, MW category, class
+mw_category_class <- select(protein_data, molecular_weight, mw_category, class)
+
+bar_chart <- mw_category_class |>
+  group_by()
+  ggplot(mw_category_class, aes(x = class, y = molecular_weight, fill = mw_category)) + 
+  geom_bar(stat = "identity", position = "dodge") + 
+  labs(x = "Protein Class", y = "Molecular Weight", fill = "MW Category")
+bar_chart
