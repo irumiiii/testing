c9758acbf67e52d286f353dee37ea8ada15e2236
Roum Lee <81roumiiii@gmail.com>
2026-07-27 09:03:53 -0700

    Performed Linear regression on protein dataset, finding best fit line to predict molecular weight using sequence length

--- knn_linear_regression.R
+++ knn_linear_regression.R
@@ -32,7 +32,7 @@ ggplot(receptor_100, aes(x = sequence_length, y = mw)) +
   labs(x = "Sequence Length", y = "Molecular Weight", title = "Sequence Length vs MW")
 #as predicted, a positive correlation
 
-#I want to predict molecular weight given a particular value of sequence length
+#I want to predict molecular weight given a particular value of sequence length. Use KNN Regression
 #Predict molecular weight of receptor with sequence length 100 taking K = 4
 predicted_mw <- receptor_data |>
   mutate(diff = abs(100 - sequence_length)) |>
@@ -94,7 +94,57 @@ receptor_summary <- receptor_best_fit |>
   bind_cols(receptor_test) |>
   metrics(truth = mw, estimate = .pred)
 
+#Same thing, this time using Linear Regression
+receptor_split <- initial_split(receptor_data, prop = 0.75, strata = mw)
+receptor_train <- training(receptor_split)
+receptor_test <- testing(receptor_split)
 
+#Model Specification
+linear_model <- linear_reg() |>
+  set_engine("lm") |>
+  set_mode("regression")
 
+#Preprocess Data
+linear_recipe <- recipe(mw ~ sequence_length, data = receptor_train) 
 
+#Fit data on workflow
+linear_workflow <- workflow()|>
+  add_recipe(linear_recipe) |>
+  add_model(linear_model) |>
+  fit(data = receptor_train)
 
+#Visualizing Model Prediction on Training Data
+receptor_preds <- linear_workflow |>
+  predict(receptor_train) |>
+  bind_cols(receptor_train)
+
+receptor_predictions <- receptor_preds |>
+  ggplot(aes(x = sequence_length, y = mw)) + 
+  geom_point(alpha = 0.4) + 
+  geom_line(aes(x = sequence_length, y = .pred), colour = "red") + 
+  xlab("Sequence Length") + 
+  ylab("Molecular Weight") + 
+  theme(text = element_text(size = 15))
+  
+#Evaluate Regression Model using Test Data
+receptor_rmse <- linear_workflow |>
+  predict(receptor_test) |>
+  bind_cols(receptor_test) |>
+  metrics(truth = mw, estimate = .pred) |>
+  filter(.metric == "rmse") |>
+  select(.estimate) |>
+  pull()
+#RMSPE is 387, indicating that model's predictions on average are off by 387 units of MW
+
+#Visualizing Model Prediction on Testing Data
+receptor_test_predictions <- linear_workflow |>
+  predict(receptor_test) |>
+  bind_cols(receptor_test) 
+
+ggplot(receptor_test_predictions, aes(x = sequence_length, y = mw)) +
+  geom_point(alpha = 0.4) + 
+  geom_line(aes(x = sequence_length, y = .pred), colour = "blue") + 
+  labs(x = "Sequence Length", y = "Molecular Weight", title = "Sequence Length vs Molecular Weigth Prediction on Testing Data") + 
+  theme(text = element_text(size = 15)) 
+
+#The equation of line of best fit is y = 11.32 + 118.92x
